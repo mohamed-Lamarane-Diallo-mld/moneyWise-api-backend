fcf0d3f8ccd4ea236d16626ce96ebc7a5dcd61ee
mohamed-Lamarane-Diallo-mld <mohamed.lamarane.diallo.7@gmail.com>
2025-09-26 21:22:43 +0000

    deploiment sur render avec docker 4

--- database/migrations/2025_09_26_011317_create_categories_table.php
+++ database/migrations/2025_09_26_011317_create_categories_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->enum('type', ['income', 'expense']); // pour différencier
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // perso par user
+            $table->unsignedBigInteger('user_id');
+            $table->string('name')->unique();
+            $table->enum('type', ['income', 'expense']);
+            // pour différencier
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
