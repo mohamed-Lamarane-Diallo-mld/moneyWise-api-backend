d1a39ab18781db5ec68e3a70acf6e6e4ecbdba7d
mohamed-Lamarane-Diallo-mld <mohamed.lamarane.diallo.7@gmail.com>
2025-09-26 16:24:22 +0000

    Integration de l'emsemble des test avec postman

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,14 +21,22 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    // public function definition(): array
+    // {
+    //     return [
+    //         'name' => fake()->name(),
+    //         'email' => fake()->unique()->safeEmail(),
+    //         'email_verified_at' => now(),
+    //         'password' => static::$password ??= Hash::make('password'),
+    //         'remember_token' => Str::random(10),
+    //     ];
+    // }
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'),
         ];
     }
 
