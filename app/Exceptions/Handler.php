ea8f2bf2b45d5a0f85450b03295d45b6b3232263
mohamed-Lamarane-Diallo-mld <mohamed.lamarane.diallo.7@gmail.com>
2025-09-27 18:47:43 +0000

    Gestion des erreur du server

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,10 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -25,6 +29,36 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+         // 404 - Route non trouvée
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Route non trouvée',
+                ], 404);
+            }
+        });
+
+        // 405 - Mauvaise méthode HTTP
+        $this->renderable(function (MethodNotAllowedHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Méthode non autorisée',
+                ], 405);
+            }
+        });
+
+        // 500 - Erreurs internes par défaut
+        $this->renderable(function (Throwable $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $e->getMessage() ?: 'Erreur interne du serveur',
+                ], 500);
+            }
         });
     }
 }
